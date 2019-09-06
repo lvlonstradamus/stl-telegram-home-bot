915a4101644bc204f7115f82269136d741cefe5c
Alexey Sosnovsky <a.sosnovsky@netris.ru>
2019-09-06 20:24:27 +0300

    change hardcode to ENV

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Telegraf = require('telegraf')
 
-const bot = new Telegraf('838117254:AAHihInYXXHm4Fo2UQuqdLmsiyCKKZnZo3E')
+const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => ctx.reply('Welcome'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
